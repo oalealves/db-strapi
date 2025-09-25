98264f9fc50af760495e50069b12987202135027
Ale Alves <alealves1209@gmail.com>
2025-09-25 20:44:57 -0300

    configurando o postgres

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,9 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  connection: {
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
+    },
   },
 });
